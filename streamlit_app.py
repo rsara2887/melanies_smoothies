bd41a1414de2658052cf75487bc9e1ebac43d36b
rsara2887 <45308641+rsara2887@users.noreply.github.com>
2026-09-21 08:30:45 +0530

    Update streamlit_app.py
    Nutrition Info

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,7 @@ if ingredients_list:
 
             st.success(f" Your Smoothie is ordered, {name_on_order}!", icon="✅")   
     #st.stop()
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
